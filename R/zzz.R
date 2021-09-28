27a3b2698b6f925187868d514db881eff485daf2
Alexey Seleznev <selesnow@mail.ru>
2021-09-28 16:22:52 +0300

    Добавил приветвенное сообщение

---
+++ R/zzz.R
@@ -0,0 +1,33 @@
+.onAttach <- function(lib, pkg,...){
+
+  packageStartupMessage(ractivecampaignWelcomeMessage())
+
+}
+
+
+ractivecampaignWelcomeMessage <- function(){
+  # library(utils)
+
+  paste0("\n",
+         "---------------------\n",
+         "Welcome to ractivecampaign version ", utils::packageDescription("ractivecampaign")$Version, "\n",
+         "\n",
+         "Author:           Alexey Seleznev (Head of analytics dept at Netpeak).\n",
+         "Telegram channel: https://t.me/R4marketing \n",
+         "YouTube channel:  https://www.youtube.com/R4marketing/?sub_confirmation=1 \n",
+         "Email:            selesnow@gmail.com\n",
+         "Site:             https://selesnow.github.io \n",
+         "Blog:             https://alexeyseleznev.wordpress.com \n",
+         "Facebook:         https://facebook.com/selesnown \n",
+         "Linkedin:         https://www.linkedin.com/in/selesnow \n",
+         "\n",
+         "Type ?ractivecampaign for the main documentation.\n",
+         "The github page is: https://github.com/selesnow/ractivecampaign/\n",
+         "\n",
+         "Suggestions and bug-reports can be submitted at: https://github.com/selesnow/ractivecampaign/issues\n",
+         "Or contact: <selesnow@gmail.com>\n",
+         "\n",
+         "\tTo suppress this message use:  ", "suppressPackageStartupMessages(library(ractivecampaign))\n",
+         "---------------------\n"
+  )
+}
