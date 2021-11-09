78816df346c074991fa5c5aaff08ae13fcebdb19
Alexey Seleznev <selesnow@mail.ru>
2021-11-09 15:19:47 +0200

    Добавлены функции запроса тегов

---
+++ R/ac_get_contact_tags.R
@@ -0,0 +1,40 @@
+#' Get contacts tags
+#'
+#' @param contact_id contact id
+#'
+#' @return tibble with contacts tags
+#' @export
+ac_get_contact_tags <- function(
+  contact_id
+){
+
+  ac_check_auth()
+
+  # send requests
+  list_data <- pblapply(contact_id, function(cid) {
+
+    ans <- GET(str_glue("{Sys.getenv('ACTIVECAMPAGN_API_URL')}/api/3/contacts/{cid}/contactTags"),
+               query = list(limit  = 100),
+               add_headers("Api-Token" = Sys.getenv('ACTIVECAMPAGN_API_TOKEN')))
+
+    data <- content(ans)
+
+    if ( status_code(ans) > 299 ) {
+      stop(data$message)
+    }
+
+    out_data <- tibble(data = data$contactTags) %>%
+                unnest_wider('data')
+
+    Sys.sleep(0.25)
+
+    return(out_data)
+
+  }
+  )
+
+  out_data <- bind_rows(list_data)
+
+  return(out_data)
+
+}
---
+++ R/ac_get_tags.R
@@ -0,0 +1,57 @@
+#' Get list of all tags
+#'
+#' @param search Filter by name of tag(s)
+#'
+#' @return tibble with tags metadata
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' tags <- ac_get_tags()
+#' }
+ac_get_tags <- function(
+  search = NULL
+) {
+
+
+  ac_check_auth()
+
+  # vars
+  is_first_iteration <- TRUE
+  limit  <- 100
+  offset <- 0
+  total  <- NA
+  res    <- list()
+
+  while ( (is.na(total) | offset <= total) | is_first_iteration  ) {
+
+    # send request
+    ans <- GET(str_glue("{Sys.getenv('ACTIVECAMPAGN_API_URL')}/api/3/tags"),
+               query = list(limit  = limit,
+                            offset = offset,
+                            search = search),
+               add_headers("Api-Token" = Sys.getenv('ACTIVECAMPAGN_API_TOKEN')))
+
+    data <- content(ans)
+
+    if ( status_code(ans) > 299 ) {
+      stop(data$message)
+    }
+
+    out_data <- tibble(data = data$tags) %>%
+                unnest_wider(data)
+
+    is_first_iteration <- FALSE
+    offset <- offset + limit
+    total  <- as.numeric(data$meta$total)
+    res <- append(res, list(out_data))
+
+    Sys.sleep(0.25)
+
+  }
+
+  res <- bind_rows(res)
+
+  return(res)
+
+}
