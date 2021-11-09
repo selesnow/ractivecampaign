d25080ee3d327f40322f8d8536c57d12d3672003
Alexey Seleznev <selesnow@mail.ru>
2021-11-09 11:41:47 +0200

    Добавил функцию запроса логов сделки

---
+++ R/ac_get_deal_activities.R
@@ -0,0 +1,49 @@
+
+#' Retrieve all activies if deals
+#'
+#' @param deal_id IDs of deal
+#'
+#' @return tibble with deal activies
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' Sys.setenv('ACTIVECAMPAGN_API_TOKEN' = "YOUR_TOKEN")
+#' Sys.setenv('ACTIVECAMPAGN_API_URL' = "https://<your-account>.api-us1.com")
+#'
+#' c_deal_activies<- ac_get_deal_activities(c(11, 54, 67))
+#' }
+ac_get_deal_activities <- function(
+  deal_id
+) {
+
+  ac_check_auth()
+
+  # send requests
+  list_data <- pblapply(deal_id, function(did) {
+
+    ans <- GET(str_glue("{Sys.getenv('ACTIVECAMPAGN_API_URL')}/api/3/deals/{did}/dealActivities"),
+               query = list(limit  = 100),
+               add_headers("Api-Token" = Sys.getenv('ACTIVECAMPAGN_API_TOKEN')))
+
+    data <- content(ans)
+
+    if ( status_code(ans) > 299 ) {
+      stop(data$message)
+    }
+
+    out_data <- tibble(data = data$dealActivities) %>%
+                unnest_wider('data')
+
+    Sys.sleep(0.25)
+
+    return(out_data)
+
+  }
+  )
+
+  out_data <- bind_rows(list_data)
+
+  return(out_data)
+
+}
