d9070a17408c869b5bce420d2224ecff041b3f09
Alexey <selesnow@gmail.com>
2021-12-06 23:15:10 +0200

    Заменил sep на collapse

--- R/ac_get_custom_deal_fields_values.R
+++ R/ac_get_custom_deal_fields_values.R
@@ -49,7 +49,7 @@ ac_get_custom_deal_fields_values <- function(
     }
 
     out_data <- tibble(data = data$dealCustomFieldData) %>%
-                unnest_wider(data, transform = list(fieldValue = function(x) paste(x, sep = ', ')))
+                unnest_wider(data, transform = list(fieldValue = function(x) paste(x, collapse = ', ')))
 
     is_first_iteration <- FALSE
     offset <- offset + limit
